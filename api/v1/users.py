d89e94bed3887022ed8879cc08f69040b7e3e88e
cattle4808 <farhodovjasur99@gmail.com>
2025-10-23 09:12:57 +0500

    add alembic

--- api/v1/users.py
+++ api/v1/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from crud import user as crud_user
-
+from core.db import get_db
 router = APIRouter()
 
 @router.get("/ping")
@@ -10,5 +10,10 @@ async def ping():
 
 @router.get("/user/{email}")
 async def get_user(email: str):
-    _ = crud_user.get_by_email(email)
-    return {"email": email}
+    _ = crud_user.get_by_email(get_db(), email)
+    return {"data": email}
+
+@router.get("/users/")
+async def get_users(skip: int, limit: int):
+    _ = crud_user.list_users(get_db(), skip=skip, limit=limit)
+    return {"data": _}
